2c6203988b8aac5314328377796f95ee02b3724b
MateuszGryska <51884692+MateuszGryska@users.noreply.github.com>
2020-05-24 11:17:39 +0200

    Project config

---
+++ src/theme/GlobalStyle.js
@@ -0,0 +1,29 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  
+  *, *::before, *::after {
+    box-sizing: border-box;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+  
+  html {
+    font-size: 62.5%; 
+  }
+  
+  body {
+    overflow-x: hidden;
+    overflow-y: scroll;
+    font-size: 1.6rem;
+    font-family: "Roboto", sans-serif;
+    padding: 0;
+    margin: 0;
+  }
+
+  a {
+  text-decoration: none;
+}
+`;
+
+export default GlobalStyle;
