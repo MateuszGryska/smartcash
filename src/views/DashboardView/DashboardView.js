2fafd731ebe19db0d033de2c13eea12d91e58fd5
MateuszGryska <51884692+MateuszGryska@users.noreply.github.com>
2020-05-25 18:59:31 +0200

    Add first cards to dashboard view

--- src/views/DashboardView/DashboardView.js
+++ src/views/DashboardView/DashboardView.js
@@ -1,7 +1,36 @@
 import React from 'react';
 
+import { makeStyles } from '@material-ui/styles';
+import { Grid } from '@material-ui/core';
+
+import SmallCard from './components/SmallCard';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    padding: theme.spacing(4),
+  },
+}));
+
 const DashboardView = () => {
-  return <h1>DashboardView</h1>;
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <Grid container spacing="4">
+        <Grid item lg={3} sm={6} xl={3} xs={12}>
+          <SmallCard title="Budget" amount="$24000" />
+        </Grid>
+        <Grid item lg={3} sm={6} xl={3} xs={12}>
+          <SmallCard title="Income" amount="$4600" />
+        </Grid>
+        <Grid item lg={3} sm={6} xl={3} xs={12}>
+          <SmallCard title="Outcome" amount="$2434" />
+        </Grid>
+        <Grid item lg={3} sm={6} xl={3} xs={12}>
+          <SmallCard title="Total" amount="$2330" />
+        </Grid>
+      </Grid>
+    </div>
+  );
 };
 
 export default DashboardView;
