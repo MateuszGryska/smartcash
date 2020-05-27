6aa73e33d34ee16fe666ee38fc31f07fb891dc29
MateuszGryska <51884692+MateuszGryska@users.noreply.github.com>
2020-05-27 18:16:27 +0200

    Make wallets view

--- src/views/WalletsView/WalletsView.js
+++ src/views/WalletsView/WalletsView.js
@@ -1,7 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { makeStyles } from '@material-ui/styles';
+import { Grid } from '@material-ui/core';
+import Toolbar from './components/Toolbar';
+import MoneyCard from './components/MoneyCard';
+
+import mockData from './data';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    padding: theme.spacing(4),
+  },
+  content: {
+    marginTop: theme.spacing(2),
+  },
+}));
 
 const WalletsView = () => {
-  return <h1>WalletsView</h1>;
+  const classes = useStyles();
+
+  const [wallets] = useState(mockData);
+  return (
+    <div className={classes.root}>
+      <Toolbar />
+      <div className={classes.content}>
+        <Grid container spacing="4">
+          {wallets.map(({ id, accountName, moneyValue, updatedAt }) => (
+            <Grid item lg={4} sm={6} xl={4} xs={12}>
+              <MoneyCard
+                accountName={accountName}
+                key={id}
+                moneyValue={moneyValue}
+                updatedAt={updatedAt}
+              />
+            </Grid>
+          ))}
+        </Grid>
+      </div>
+    </div>
+  );
 };
 
 export default WalletsView;
---
+++ src/views/WalletsView/components/Toolbar.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/styles';
+import { Button } from '@material-ui/core';
+
+import SearchInput from '../../../components/SearchInput/SearchInput';
+
+const useStyles = makeStyles((theme) => ({
+  root: {},
+  row: {
+    height: '42px',
+    display: 'flex',
+    alignItems: 'center',
+    marginTop: theme.spacing(1),
+  },
+  spacer: {
+    flexGrow: 1,
+  },
+  importButton: {
+    marginRight: theme.spacing(1),
+  },
+  exportButton: {
+    marginRight: theme.spacing(1),
+  },
+  searchInput: {
+    marginRight: theme.spacing(1),
+  },
+}));
+
+const Toolbar = () => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <div className={classes.row}>
+        <SearchInput className={classes.searchInput} placeholder="Search..." />
+        <div className={classes.spacer} />
+        <Button color="primary" variant="contained">
+          New wallet
+        </Button>
+      </div>
+      <div className={classes.row} />
+    </div>
+  );
+};
+
+export default Toolbar;
