c8b308da067335184440ddb0dc3fcd16f9a96435
MateuszGryska <51884692+MateuszGryska@users.noreply.github.com>
2020-05-27 17:31:07 +0200

    Make expenses view

--- src/views/ExpensesView/components/ExpensesList.js
+++ src/views/ExpensesView/components/ExpensesList.js
@@ -5,7 +5,6 @@ import {
   Card,
   CardActions,
   CardContent,
-  Button,
   Checkbox,
   Divider,
   Table,
@@ -16,14 +15,14 @@ import {
   Tooltip,
   TableSortLabel,
   IconButton,
+  TablePagination,
 } from '@material-ui/core';
-import ArrowRightIcon from '@material-ui/icons/ArrowRight';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
 import EditIcon from '@material-ui/icons/Edit';
 
 import mockData from './data';
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(() => ({
   root: {},
   content: {
     padding: 0,
@@ -35,9 +34,6 @@ const useStyles = makeStyles((theme) => ({
     display: 'flex',
     alignItems: 'center',
   },
-  avatar: {
-    marginRight: theme.spacing(2),
-  },
   actions: {
     justifyContent: 'flex-end',
   },
@@ -45,7 +41,53 @@ const useStyles = makeStyles((theme) => ({
 
 const ExpensesList = () => {
   const [bilanceItems] = useState(mockData);
+  const [selectedItems, setSelectedItems] = useState([]);
+  const [rowsPerPage, setRowsPerPage] = useState(10);
+  const [page, setPage] = useState(0);
+
   const classes = useStyles();
+  /* eslint-disable */
+  const handleSelectAll = (event) => {
+    let selectedItems;
+
+    if (event.target.checked) {
+      selectedItems = bilanceItems.map((data) => data.id);
+    } else {
+      selectedItems = [];
+    }
+
+    setSelectedItems(selectedItems);
+  };
+  /* eslint-enable */
+
+  const handleSelectOne = (event, id) => {
+    const selectedIndex = selectedItems.indexOf(id);
+    let newSelectedItems = [];
+
+    if (selectedIndex === -1) {
+      newSelectedItems = newSelectedItems.concat(selectedItems, id);
+    } else if (selectedIndex === 0) {
+      newSelectedItems = newSelectedItems.concat(selectedItems.slice(1));
+    } else if (selectedIndex === selectedItems.length - 1) {
+      newSelectedItems = newSelectedItems.concat(selectedItems.slice(0, -1));
+    } else if (selectedIndex > 0) {
+      newSelectedItems = newSelectedItems.concat(
+        selectedItems.slice(0, selectedIndex),
+        selectedItems.slice(selectedIndex + 1),
+      );
+    }
+
+    setSelectedItems(newSelectedItems);
+  };
+
+  const handlePageChange = (event, currentPage) => {
+    setPage(currentPage);
+  };
+
+  const handleRowsPerPageChange = (event) => {
+    setRowsPerPage(event.target.value);
+  };
+
   return (
     <Card className={classes.root}>
       <CardContent className={classes.content}>
@@ -53,8 +95,15 @@ const ExpensesList = () => {
           <div className={classes.inner}>
             <Table>
               <TableHead>
-                <TableCell>
-                  <Checkbox />
+                <TableCell padding="checkbox">
+                  <Checkbox
+                    checked={selectedItems.length === bilanceItems.length}
+                    color="primary"
+                    indeterminate={
+                      selectedItems.length > 0 && selectedItems.length < bilanceItems.length
+                    }
+                    onChange={handleSelectAll}
+                  />
                 </TableCell>
                 <TableCell sortDirection="desc">
                   <Tooltip enterDelay={300} title="Sort">
@@ -70,26 +119,38 @@ const ExpensesList = () => {
                 <TableCell>Actions</TableCell>
               </TableHead>
               <TableBody>
-                {bilanceItems.map((item) => (
-                  <TableRow hover key={item.id}>
-                    <TableCell>
-                      <Checkbox />
-                    </TableCell>
-                    <TableCell>{item.date}</TableCell>
-                    <TableCell>{item.name}</TableCell>
-                    <TableCell>{item.wallet}</TableCell>
-                    <TableCell>{item.price}</TableCell>
-                    <TableCell>{item.category}</TableCell>
-                    <TableCell>
-                      <IconButton edge="end" size="medium">
-                        <MoreVertIcon />
-                      </IconButton>
-                      <IconButton edge="end" size="medium">
-                        <EditIcon />
-                      </IconButton>
-                    </TableCell>
-                  </TableRow>
-                ))}
+                {bilanceItems
+                  .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                  .map((item) => (
+                    <TableRow
+                      className={classes.tableRow}
+                      hover
+                      key={item.id}
+                      selected={selectedItems.indexOf(item.id) !== -1}
+                    >
+                      <TableCell padding="checkbox">
+                        <Checkbox
+                          checked={selectedItems.indexOf(item.id) !== -1}
+                          color="primary"
+                          onChange={(event) => handleSelectOne(event, item.id)}
+                          value="true"
+                        />
+                      </TableCell>
+                      <TableCell>{item.date}</TableCell>
+                      <TableCell>{item.name}</TableCell>
+                      <TableCell>{item.wallet}</TableCell>
+                      <TableCell>{item.price}</TableCell>
+                      <TableCell>{item.category}</TableCell>
+                      <TableCell>
+                        <IconButton edge="end" size="medium">
+                          <MoreVertIcon />
+                        </IconButton>
+                        <IconButton edge="end" size="medium">
+                          <EditIcon />
+                        </IconButton>
+                      </TableCell>
+                    </TableRow>
+                  ))}
               </TableBody>
             </Table>
           </div>
@@ -97,9 +158,15 @@ const ExpensesList = () => {
       </CardContent>
       <Divider />
       <CardActions className={classes.actions}>
-        <Button size="large" color="primary" variant="text">
-          View all <ArrowRightIcon />
-        </Button>
+        <TablePagination
+          component="div"
+          count={bilanceItems.length}
+          onChangePage={handlePageChange}
+          onChangeRowsPerPage={handleRowsPerPageChange}
+          page={page}
+          rowsPerPage={rowsPerPage}
+          rowsPerPageOptions={[5, 10, 25]}
+        />
       </CardActions>
     </Card>
   );
