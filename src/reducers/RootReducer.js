c090bc01126b4549de3be8419134aff9ff65deab
MateuszGryska <51884692+MateuszGryska@users.noreply.github.com>
2020-07-15 20:33:31 +0200

    Fix bug with state after logout

--- src/reducers/RootReducer.js
+++ src/reducers/RootReducer.js
@@ -2,9 +2,18 @@ import { combineReducers } from 'redux';
 import authReducer from 'reducers/authReducer';
 import itemsReducer from 'reducers/itemsReducer';
 
-const RootReducer = combineReducers({
+const appReducer = combineReducers({
   auth: authReducer,
   items: itemsReducer,
 });
+
+const RootReducer = (state, action) => {
+  if (action.type === 'LOG_OUT_USER') {
+    // eslint-disable-next-line
+    state = undefined;
+  }
+
+  return appReducer(state, action);
+};
 
 export default RootReducer;
